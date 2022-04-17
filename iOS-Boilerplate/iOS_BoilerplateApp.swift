f92038322fb79d2d7ea2adb8d98d0e200bc68816
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-04-18 07:58:24 +0800

    Initial Commit

---
+++ iOS-Boilerplate/iOS_BoilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BoilerplateApp.swift
+//  iOS-Boilerplate
+//
+//  Created by Neil Francis Hipona on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BoilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
